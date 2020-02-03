c5bf2bc049f5c117dc7d1df28ccaeec3094cfa90
NekiCoule <webermax.mail@gmail.com>
2020-02-03 23:07:47 +0100

    Rotation "works"
    Rotation need to be tuned
    Need to fix grab (currently grabbing the tile from anywhere)

--- Scenes/Cards/Script/Tile.gd
+++ Scenes/Cards/Script/Tile.gd
@@ -2,23 +2,32 @@ extends Area2D
 
 var offset : Vector2
 var grab : bool
-
+var has_parent : bool
 
 # ---- READY ----
 
 func _ready():
 	grab = false
+	has_parent = get_parent() is Card
 	
-
 # ---- INPUT ----
 
 func _input(event):
 	if Input.is_action_just_pressed("grab"):
-		if get_parent() is Card:
+		if has_parent:
 			offset = get_parent().get_global_position() - get_viewport().get_mouse_position()
 		else:
 			offset = get_global_position() - get_viewport().get_mouse_position()
 		grab = true
+		
+	if Input.is_action_just_pressed("rotate_card_clock") && Input.is_action_pressed("grab"):
+		if has_parent:
+			get_parent().set_rotation(get_parent().get_rotation() + 90)
+	
+	if Input.is_action_just_pressed("rotate_card_anti_clock") && Input.is_action_pressed("grab"):
+		if has_parent:
+			get_parent().set_rotation(get_parent().get_rotation() - 90)
+			
 	if Input.is_action_just_released("grab"):
 		grab = false
 
