8cf8ea6caa6ef75be2fe4dab7147f70870ac8ca8
NekiCoule <webermax.mail@gmail.com>
2020-02-04 09:44:28 +0100

    Drag and Drop works
    Removed unnecessary ColorRect in card scenes, drag and drop works properly.
    Rotation works too but sometimes the card rotate twice in one input
    Next strep: fix rotation

--- Scenes/Cards/Script/Tile.gd
+++ Scenes/Cards/Script/Tile.gd
@@ -3,36 +3,52 @@ extends Area2D
 var offset : Vector2
 var grab : bool
 var has_parent : bool
+var overlap : bool
 
 # ---- READY ----
 
 func _ready():
 	grab = false
+	overlap = false
 	has_parent = get_parent() is Card
 	
 # ---- INPUT ----
 
-func _input(event):
-	if Input.is_action_just_pressed("grab"):
-		if has_parent:
-			offset = get_parent().get_global_position() - get_viewport().get_mouse_position()
-		else:
-			offset = get_global_position() - get_viewport().get_mouse_position()
-		grab = true
-		
-	if Input.is_action_just_pressed("rotate_card_clock") && Input.is_action_pressed("grab"):
-		if has_parent:
-			get_parent().set_rotation(get_parent().get_rotation() + 90)
+func _on_Tile_mouse_entered():
+	print("overlap")
+	overlap = true
 	
-	if Input.is_action_just_pressed("rotate_card_anti_clock") && Input.is_action_pressed("grab"):
-		if has_parent:
-			get_parent().set_rotation(get_parent().get_rotation() - 90)
-			
-	if Input.is_action_just_released("grab"):
-		grab = false
+func _on_Tile_mouse_exited():
+	print("end overlap")
+	if !grab:
+		overlap = false
+
+
+func _input(event):
+	
+	if overlap:
+		if Input.is_action_just_pressed("grab"):
+			if has_parent:
+				offset = get_parent().get_global_position() - get_viewport().get_mouse_position()
+			else:
+				offset = get_global_position() - get_viewport().get_mouse_position()
+			grab = true
+		
+		if Input.is_action_just_pressed("rotate_card_clock") && Input.is_action_pressed("grab"):
+			if has_parent:
+				get_parent().set_rotation_degrees(get_parent().get_rotation_degrees() + 90)
+		
+		if Input.is_action_just_pressed("rotate_card_anti_clock") && Input.is_action_pressed("grab"):
+			if has_parent:
+				get_parent().set_rotation_degrees(get_parent().get_rotation_degrees() - 90)
+		
+		if Input.is_action_just_released("grab"):
+			grab = false
+			overlap = false
 
 
 # ---- FUNCTIONS ----
+
 
 # function : deactivate_drag_collision
 # parameters : None
@@ -51,4 +67,3 @@ func _process(delta):
 			get_parent().set_global_position(get_viewport().get_mouse_position() + offset)
 		else:
 			set_global_position(get_viewport().get_mouse_position() + offset)
-	
